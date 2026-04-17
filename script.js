3532c61fb11dfc8d8e6f8e20cae940beba77b909
hasini-01-08 <hasinibottupally96@gmail.com>
2026-04-18 00:00:31 +0530

    Enhance bar rendering with number labels
    Added number labels to the rendered bars in the array visualization.

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-let array = [];
-let steps = [];
-let currentStep = 0;
-let speed = 50;
-
-// Generate random array
-function generateArray() {
-  array = [];
-  for (let i = 0; i < 20; i++) {
-    array.push(Math.floor(Math.random() * 100));
-  }
-  renderArray(array);
-}
-
-// Render bars
 function renderArray(arr) {
   const container = document.getElementById("array-container");
   container.innerHTML = "";
@@ -21,49 +6,13 @@ function renderArray(arr) {
     const bar = document.createElement("div");
     bar.style.height = value * 3 + "px";
     bar.classList.add("bar");
+
+    // Create number label
+    const label = document.createElement("span");
+    label.innerText = value;
+    label.classList.add("label");
+
+    bar.appendChild(label);
     container.appendChild(bar);
   });
 }
-
-// Bubble sort steps
-function bubbleSortSteps(arr) {
-  let a = [...arr];
-  let steps = [];
-
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < a.length - i - 1; j++) {
-      if (a[j] > a[j + 1]) {
-        [a[j], a[j + 1]] = [a[j + 1], a[j]];
-        steps.push([...a]);
-      }
-    }
-  }
-  return steps;
-}
-
-// Start sorting
-function startBubble() {
-  steps = bubbleSortSteps(array);
-  currentStep = 0;
-}
-
-// Next step
-function nextStep() {
-  if (currentStep < steps.length) {
-    renderArray(steps[currentStep]);
-    currentStep++;
-  }
-}
-
-// Previous step
-function prevStep() {
-  if (currentStep > 0) {
-    currentStep--;
-    renderArray(steps[currentStep]);
-  }
-}
-
-// Speed control
-document.getElementById("speed").addEventListener("input", (e) => {
-  speed = 101 - e.target.value;
-});
